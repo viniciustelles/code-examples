9ce49784f6a91304d5d3badca955b34868186775
Vinicius Telles <telles.code@gmail.com>
2023-09-23 23:26:59 -0300

    feature: init  catalog service

---
+++ catalog-servie/src/main/java/com/crud/catalog/CatalogServieApplication.java
@@ -0,0 +1,13 @@
+package com.crud.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServieApplication.class, args);
+	}
+
+}
